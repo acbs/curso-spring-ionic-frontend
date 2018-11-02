6ac814c6fa0c355f24e788e452c15e5c953315f7
André Carlos <andre.carlos1993@hotmail.com>
2018-11-02 18:29:31 -0300

    Tela inicial

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
 
 // Informa q essa clase é uma página e poderá ser referenciada pelo nome entre aspas
-// Tornando ela mais flexivel para trabalhar no modo Lazy loading
+// Tornando ela mais flexível para trabalhar no modo Lazy loading
 @IonicPage()
 // Faz essa classe ser o controlador da view
 @Component({
