81bb28ae5bf7ad536e5f25f1b4fd52a074cc2722
André Carlos <andre.carlos1993@hotmail.com>
2018-11-07 22:55:34 -0300

    Obtendo os dados do formulário de login

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
 import { MenuController } from 'ionic-angular/components/app/menu-controller';
+import { CredenciasDTO } from '../../models/credencias.dto';
 
 // Informa q essa clase é uma página e poderá ser referenciada pelo nome entre aspas
 // Tornando ela mais flexível para trabalhar no modo Lazy loading
@@ -11,6 +12,11 @@ import { MenuController } from 'ionic-angular/components/app/menu-controller';
   templateUrl: 'home.html'
 })
 export class HomePage {
+
+  creds: CredenciasDTO = {
+    email: "",
+    senha: ""
+  }
 
   constructor(public navCtrl: NavController, public menu: MenuController) {
 
@@ -29,6 +35,7 @@ export class HomePage {
   login() {
     // Chamando outra página, o push ele vai empilhar uma página na outra (Colocando o btn voltar)
     // setRoot chama outra tela sem empilhar
+    console.log(this.creds);
     this.navCtrl.setRoot('CategoriasPage');
   }
 }
