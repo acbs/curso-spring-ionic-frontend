d77c5dd4e799b607c73e093992ce0e0eea6d5504
André Carlos <andre.carlos1993@hotmail.com>
2018-11-24 11:04:00 -0300

    Tela de confirmação de pedido

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -11,6 +11,10 @@ export class ClienteService {
   constructor(public http: HttpClient, public storage: StorageService) {
   }
 
+  findById(id: string) {
+    return this.http.get(`${API_CONFIG.baseURL}/clientes/${id}`);
+  }
+
   findByEmail(email: string) : Observable<ClienteDTO> {
     return this.http.get<ClienteDTO>(`${API_CONFIG.baseURL}/clientes/email?value=${email}`);
   }
